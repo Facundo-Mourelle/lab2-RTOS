bfaa41da3209d24da47cb60839618359d11ed2a4
Facundo Mourelle <facundo.mourelle@mi.unc.edu.ar>
2026-05-06 22:45:21 -0300

    desafio4

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,26 +1,26 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2026 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
-#include "usb_host.h"
 #include "gpio.h"
+#include "usb_host.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -34,6 +34,12 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define LED1 GPIO_PIN_12
+#define LED2 GPIO_PIN_13
+#define LED_PORT GPIOD
+
+#define BTN_PIN GPIO_PIN_0
+#define BTN_PORT GPIOA
 
 /* USER CODE END PD */
 
@@ -45,7 +51,9 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-
+static QueueHandle_t Cola_Global = NULL;
+static SemaphoreHandle_t Sem_LeidoA = NULL;
+static SemaphoreHandle_t Sem_LeidoB = NULL;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -57,15 +65,76 @@ void MX_FREERTOS_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+void vTareaProductora(void *pvParameters) {
+  uint16_t contador = 0;
+  uint8_t estado_ant = GPIO_PIN_RESET;
+  for (;;) {
+    uint8_t estado_act = HAL_GPIO_ReadPin(BTN_PORT, BTN_PIN);
+    if (estado_act == GPIO_PIN_RESET && estado_ant == GPIO_PIN_SET) {
+      vTaskDelay(pdMS_TO_TICKS(50));
+      contador++;
+      xQueueOverwrite(Cola_Global, &contador);
+    }
+    estado_ant = estado_act;
+    vTaskDelay(pdMS_TO_TICKS(10));
+  }
+}
 
+void vTareaA(void *pvParameters) {
+  uint16_t valor;
+  for (;;) {
+    // Blocked hasta haber datos, pero no los extrae
+    if (xQueuePeek(Cola_Global, &valor, portMAX_DELAY) == pdTRUE) {
+      if (valor % 2 == 0) {
+        for (int i = 0; i < 3; i++) {
+          HAL_GPIO_WritePin(LED_PORT, LED1, GPIO_PIN_SET);
+          vTaskDelay(pdMS_TO_TICKS(200));
+          HAL_GPIO_WritePin(LED_PORT, LED1, GPIO_PIN_RESET);
+          vTaskDelay(pdMS_TO_TICKS(200));
+        }
+      }
+      xSemaphoreGive(Sem_LeidoA);
+      taskYIELD();
+    }
+  }
+}
+
+void vTareaB(void *pvParameters) {
+  uint16_t valor;
+  for (;;) {
+    // Blocked hasta haber datos, pero no los extrae
+    if (xQueuePeek(Cola_Global, &valor, portMAX_DELAY) == pdTRUE) {
+      if (valor % 2 != 0) {
+        for (int i = 0; i < 3; i++) {
+          HAL_GPIO_WritePin(LED_PORT, LED2, GPIO_PIN_SET);
+          vTaskDelay(pdMS_TO_TICKS(200));
+          HAL_GPIO_WritePin(LED_PORT, LED2, GPIO_PIN_RESET);
+          vTaskDelay(pdMS_TO_TICKS(200));
+        }
+      }
+      xSemaphoreGive(Sem_LeidoB);
+      taskYIELD();
+    }
+  }
+}
+
+void vTareaLimpieza(void *pvParameters) {
+  uint16_t valor_descartado;
+  for (;;) {
+    xSemaphoreTake(Sem_LeidoA, portMAX_DELAY);
+    xSemaphoreTake(Sem_LeidoB, portMAX_DELAY);
+    // queue deberia siempre tener un dato al llegar aca
+    xQueueReceive(Cola_Global, &valor_descartado, 0);
+    // queue ahora vacia
+  }
+}
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
 
   /* USER CODE BEGIN 1 */
 
@@ -73,7 +142,8 @@ int main(void)
 
   /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick.
+   */
   HAL_Init();
 
   /* USER CODE BEGIN Init */
@@ -90,7 +160,19 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
+  Cola_Global = xQueueCreate(1, sizeof(uint16_t));
+  if (Cola_Global != NULL) {
 
+    /* Semáforos binarios (estado inicial = no disponible) */
+    Sem_LeidoA = xSemaphoreCreateBinary();
+    Sem_LeidoB = xSemaphoreCreateBinary();
+    configASSERT(Sem_LeidoA != NULL && Sem_LeidoB != NULL);
+
+    xTaskCreate(vTareaProductora, "Productora", 128, NULL, 3, NULL);
+    xTaskCreate(vTareaA, "TareaA", 128, NULL, 2, NULL);
+    xTaskCreate(vTareaB, "TareaB", 128, NULL, 2, NULL);
+    xTaskCreate(vTareaLimpieza, "Limpieza", 128, NULL, 1, NULL);
+  }
   /* USER CODE END 2 */
 
   /* Call init function for freertos objects (in cmsis_os2.c) */
@@ -103,8 +185,7 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+  while (1) {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -113,22 +194,21 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
@@ -137,22 +217,20 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLN = 336;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 7;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+                                RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-  {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
     Error_Handler();
   }
 }
@@ -162,32 +240,30 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1)
-  {
+  while (1) {
   }
   /* USER CODE END Error_Handler_Debug */
 }
 #ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line) {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* User can add his own implementation to report the file name and line
+     number, ex: printf("Wrong parameters value: file %s on line %d\r\n", file,
+     line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
